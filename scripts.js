ea4bb838fb273b5dcc64a809d80bbd0496d9e7aa
Konrad Horber <konrad.horber@gmail.com>
2024-03-01 21:15:12 +0100

    Create rock paper scissors game
    Implement full logic including screen print from rock apper scissors
    game

--- scripts.js
+++ scripts.js
@@ -1,9 +1,67 @@
 function getComputerChoice() {
   const computer_choice = [
-    "Rock",
-    "Paper",
-    "Scissor"
+    "rock",
+    "paper",
+    "scissors"
   ];
-  return Math.trunc(Math.random()*3);
+  return computer_choice[Math.floor(Math.random() * 2.99)];
 }
-console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+  let outcome = 0;
+  // compare and select
+  if (playerSelection == 'rock') {
+    if (computerSelection == 'rock') {
+      outcome = 'tied';
+    }
+    else if (computerSelection == 'paper') {
+      outcome = 'lost';
+    }
+    else if (computerSelection == 'scissors') {
+      outcome = 'won';
+    };
+  };
+
+  if (playerSelection == 'paper') {
+    if (computerSelection == 'rock') {
+      outcome = 'won';
+    }
+    else if (computerSelection == 'paper') {
+      outcome = 'tied';
+    }
+    else if (computerSelection == 'scissors') {
+      outcome = 'lost';
+    };
+  };
+
+  if (playerSelection == 'scissors') {
+    if (computerSelection == 'rock') {
+      outcome = 'lost';
+    }
+    else if (computerSelection == 'paper') {
+      outcome = 'won';
+    }
+    else if (computerSelection == 'scissors') {
+      outcome = 'tied';
+    };
+  };
+
+  if (outcome == 'won') {
+    console.log(`You ${outcome}! ${playerSelection} beats ${computerSelection}!`)
+  }
+  else if (outcome == 'lost') {
+    console.log(`You ${outcome}! ${playerSelection} is beaten by ${computerSelection}!`)
+  }
+  else if (outcome == 'tied') {
+    console.log(`You ${outcome}! ${playerSelection} is even with ${computerSelection}!`)
+  }
+  else {console.log('error code 1')};
+  return
+}
+
+for (let i = 0; i < 5; i++) {
+  let playerSelection = prompt('Please select rock, paper, scissors: ').toLowerCase();
+  let computerSelection = getComputerChoice();
+
+  playRound(playerSelection, computerSelection);
+}
